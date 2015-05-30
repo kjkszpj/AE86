5f818b79411fdd83fdf8e7bf51a81211f7cf12c9
Peijie You <you_peijie@yahoo.com>
2015-05-30 11:42:14 +0800

    开始使用PyCharm

--- core/pipe/main.py
+++ core/pipe/main.py
@@ -1,13 +1,14 @@
 # -*- coding: cp936 -*-
-def ALU(aluA=1, aluB=1, aluFun=0):
-    if (aluFun == 0):   return aluA + aluB;
-    elif (aluFun == 1): return aluB - aluA; #order right? aluB-aluA / aluA-aluB
-    elif (aluFun == 2): return aluA & aluB;
-    elif (aluFun == 3): return aluA ^ aluB;
-    #what about condition code? implement here/new function
-    
-    print "aluFun error: %d" %(aluFun);
-    return 0;
+def alu(alu_a=1, alu_b=1, alu_fun=0):
+    #  order right? aluB-aluA / aluA-aluB
+    if alu_fun == 0:   return alu_a + alu_b
+    elif alu_fun == 1: return alu_b - alu_a
+    elif alu_fun == 2: return alu_a & alu_b
+    elif alu_fun == 3: return alu_a ^ alu_b
+
+    #  what about condition code? implement here/new function
+    print "aluFun error: %d" % alu_fun
+    return 0
 
 if __name__ == "__main__":
-    print ALU(1, 1, 1);
+    print ALU(1, 1, 1)
