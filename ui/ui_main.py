dc760a5671e06b3023abc08b690c2d6d347190de
Peijie You <you_peijie@yahoo.com>
2015-06-05 13:09:53 +0800

    加入about功能
    学会建一个简单的对话框，并成功关闭
    应该在ui_main里面connect好，实际的函数写在component/目录里面

--- ui/ui_main.py
+++ ui/ui_main.py
@@ -4,6 +4,7 @@ import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 from total import Ui_total
+from component.AboutDialog import *
 
 
 class Widow(QtGui.QMainWindow):
@@ -19,31 +20,14 @@ class Widow(QtGui.QMainWindow):
 
     #   about
     def run_about(self):
-        dialog = QtGui.QDialog(parent = self)
-        #   TODO font
-        dialog.setWindowTitle('AE86')
-        about_info = QtGui.QLabel(u'Brought to you by: 游沛杰\n'
-                                  u'Email: 13307130325@fudan.edu.cn\n'
-                                  u'View on Github:https://github.com/kjkszpj/AE86\n')
-        button_ok = QtGui.QPushButton(u'确认')
-        #   TODO connect button_ok
-        #   TODO 设置确认键的大小
-        #   构造layout
-        about_info.move(50, 50)
-        button_ok.move(233, 233)
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(about_info)
-        layout.addWidget(button_ok)
-        layout.setMargin(33)
-        #   显示框框
-        dialog.setLayout(layout)
-        dialog.exec_()
+        AboutDialog(parent = self)
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    ex = Widow()
+    u = Widow()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
