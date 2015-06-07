fd8cc52d7576c36cab340ca902b43eb3dd95f460
Peijie You <you_peijie@yahoo.com>
2015-06-07 10:35:58 +0800

    优化文件对话框QFileDialog的标题和支持类型

--- ui/component/load_instruction.py
+++ ui/component/load_instruction.py
@@ -7,6 +7,6 @@ sys.path.append('C:\\Users\\You\\Documents\\GitHub\\AE86\\accessory')
 from load_yo import load_yo
 
 def load_instruction(ui):
-    file_name = QtGui.QFileDialog.getOpenFileName(ui, ui.tr('Open instruction file'), QtCore.QString())
+    file_name = QtGui.QFileDialog.getOpenFileName(ui, ui.tr('Open instruction file'), "", QtCore.QString('*.yo'))
     load_yo(file_name)
     QtGui.QMessageBox.information(ui, "It works!", u"成功载入汇编文件%s" % file_name)
